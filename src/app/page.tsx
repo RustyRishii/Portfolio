24fbd2294e443f3c9034d462ffe30da816e648a8
RustyRishii <rishikeshbidkar04@gmail.com>
2025-08-19 14:05:58 +0530

    Added Github & LinkedIn linls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,36 +25,36 @@ export default function Home() {
           />
           Twitter
         </a>
-        {/* <a
+        <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/RustyRishii"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/github.svg"
             alt="Window icon"
-            width={16}
-            height={16}
+            width={20}
+            height={20}
           />
-          Examples
+          Github
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/rishikesh-bidkar-3452a1202/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+            src="/linkedin.svg"
+            alt="Linkedin icon"
+            width={20}
+            height={20}
           />
-          Go to nextjs.org →
-        </a> */}
+          LinkedIn
+        </a>
       </footer>
     </div>
   );
